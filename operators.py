cf5c55ad09a809689eb814e1b798767ea05aa7f3
Robinologist <112644607+Robinologist@users.noreply.github.com>
2022-09-21 08:22:46 -0400

    Commented out Operators file to signify symbol meanings

--- operators.py
+++ operators.py
@@ -1,3 +1,27 @@
+#The + - * / signs do what you'd expect
 print(2 + 3)
 print(2 - 3)
 print(2 * 3)
+print(9 * 5)
+print(2 / 5)
+
+print("")
+
+#The ** is used similar to a carat ^ to show exponents
+print(2 ** 3)
+print(3 ** 2)
+
+print("")
+
+#The // divides but rounds down || (12 // 5) = (12/5) = (2.4) -> rounded = 2
+print(12 // 5)
+print(27 // 4)
+print(2 // 5)
+
+print("")
+
+#The % sign gives you the remainder after dividing || Even number check: if (X % 2)=1, number is odd, else number is even
+print(5 % 2)
+print(9 % 5)
+print(6 % 6)
+print(2 % 7)
